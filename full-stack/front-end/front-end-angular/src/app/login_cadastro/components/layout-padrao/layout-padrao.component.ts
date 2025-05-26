fc28d5a574b71e8c9d3fc8f38bac6dfbd1dd9d04
Carlos Eduardo <eduardorodrigues@gmail.com>
2025-05-26 15:54:14 -0300

    alteracoes login e cadastro

--- full-stack/front-end/front-end-angular/src/app/login_cadastro/components/layout-padrao/layout-padrao.component.ts
+++ full-stack/front-end/front-end-angular/src/app/login_cadastro/components/layout-padrao/layout-padrao.component.ts
@@ -2,26 +2,23 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-layout-padrao',
-  imports: [],
   templateUrl: './layout-padrao.component.html',
-  styleUrl: './layout-padrao.component.scss'
+  styleUrls: ['./layout-padrao.component.scss']
 })
 export class LayoutPadraoComponent {
-
-  @Input() title: string = "";
-  @Input() primaryBtnText: string = "";
-  @Input() secondaryBtnText: string = "";
+  @Input() title: string = '';
+  @Input() primaryBtnText: string = '';
+  @Input() secondaryBtnText: string = '';
   @Input() disablePrimaryBtn: boolean = true;
-  @Output("submit") onSubmit = new EventEmitter();
 
-  @Output("navigate") onNavigate = new EventEmitter();
+  @Output() navigate = new EventEmitter<void>();
+  @Output() submit = new EventEmitter<void>();
 
-  submit(){
-    this.onSubmit.emit();
+  onNavigate() {
+    this.navigate.emit();
   }
 
-  navigate(){
-    this.onNavigate.emit();
+  onSubmit() {
+    this.submit.emit();
   }
-
 }
--- full-stack/front-end/front-end-angular/src/app/login_cadastro/pages/signup/signup.component.ts
+++ full-stack/front-end/front-end-angular/src/app/login_cadastro/pages/signup/signup.component.ts
@@ -2,57 +2,59 @@ import { Component } from '@angular/core';
 import {
   FormControl,
   FormGroup,
-  ReactiveFormsModule,
   Validators,
+  AbstractControl,
+  ValidationErrors,
+  ReactiveFormsModule,
 } from '@angular/forms';
-import { LoginService } from '../../services/login.service';
 import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
 import { PrimeiroInputComponent } from '../../components/primeiro-input/primeiro-input.component';
 import { LayoutPadraoComponent } from '../../components/layout-padrao/layout-padrao.component';
 
-interface SignupForm {
-  name: FormControl;
-  email: FormControl;
-  password: FormControl;
-  passwordConfirm: FormControl;
+function passwordMatchValidator(formGroup: AbstractControl): ValidationErrors | null {
+  const senha = formGroup.get('senha')?.value;
+  const confSenha = formGroup.get('confSenha')?.value;
+  return senha === confSenha ? null : { passwordMismatch: true };
 }
 
 @Component({
   selector: 'app-signup',
+  standalone: true,
   imports: [LayoutPadraoComponent, ReactiveFormsModule, PrimeiroInputComponent],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss',
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
-  signupForm!: FormGroup<SignupForm>;
+  signupForm: FormGroup;
 
   constructor(private router: Router, private loginService: LoginService) {
-    this.signupForm = new FormGroup({
-      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(6),
-      ]),
-      passwordConfirm: new FormControl('', [
-        Validators.required,
-        Validators.minLength(6),
-      ]),
-    });
+    this.signupForm = new FormGroup(
+      {
+        nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        senha: new FormControl('', [Validators.required, Validators.minLength(6)]),
+        confSenha: new FormControl('', [Validators.required]),
+      },
+      { validators: passwordMatchValidator }
+    );
   }
 
   submit() {
-    this.loginService
-      .signup(
-        this.signupForm.value.name,
-        this.signupForm.value.email,
-        this.signupForm.value.password
-      )
-      .subscribe({
-        next: () => window.alert('Cadastro feito com sucesso!'),
-        error: () =>
-          window.alert('Erro inesperado! Tente novamente mais tarde'),
-      });
+    if (this.signupForm.invalid) {
+      this.signupForm.markAllAsTouched();
+      return;
+    }
+
+    const { nome, email, senha } = this.signupForm.value;
+
+    this.loginService.signup(nome, email, senha).subscribe({
+      next: () => {
+        alert('Cadastro feito com sucesso!');
+        this.router.navigate(['login']);
+      },
+      error: () => alert('Erro ao cadastrar. Tente novamente.'),
+    });
   }
 
   navigate() {
